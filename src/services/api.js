740fb060d4542c33239dd85b2c979e070015e894
Emanuele Maria <emanuuuelem@gmail.com>
2025-06-24 22:48:29 -0300

    Update API base URL to point to the correct production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = "https://academia-augusto.vercel.app";
+const apiURL = "https://academia-augusto.onrender.com";
 
 export default function api() {
   return axios.create({ baseURL: apiURL });
